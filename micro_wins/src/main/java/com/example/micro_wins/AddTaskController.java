53fb8d32e1b3665e814d3193c463cabac2b7cf90
Nyamdavaa2k01 <nyamdavaa2k01@gmail.com>
2022-05-01 21:49:06 +0800

    add task view

---
+++ micro_wins/src/main/java/com/example/micro_wins/AddTaskController.java
@@ -0,0 +1,68 @@
+package com.example.micro_wins;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class AddTaskController {
+
+    @FXML
+    private Button addReminderBtn;
+
+    @FXML
+    private Button addTaskBtn;
+
+    @FXML
+    private AnchorPane addTaskView;
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private Button setPriorityBtn;
+
+    @FXML
+    private Button setProjectBtn;
+
+    @FXML
+    private Button setTimeBtn;
+
+    @FXML
+    private TextField taskDescriptionTxt;
+
+    @FXML
+    private TextField taskNameTxt;
+
+    @FXML
+    void addReminder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addTask(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setPrioirty(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setProject(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setTime(ActionEvent event) {
+
+    }
+
+}
